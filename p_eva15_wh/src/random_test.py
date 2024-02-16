b384ebfc4a3c24c934cf0f2a121258578b598473
Hiroto <c1115332@planet.kanazawa-it.ac.jp>
2024-02-16 07:54:24 +0000

    update p_eva15

---
+++ p_eva15_wh/src/random_test.py
@@ -0,0 +1,62 @@
+
+import ramdom
+
+
+
+class Eva15():
+    # 左打ち（1/319で当たる。）
+    # 
+    current_credit = 20000
+
+    def __init__(self, init_balance, ball_price=4):
+        self.balance = init_balance # First total credits
+        self.ball_price = ball_price
+        self.balls = init_balance/ball_price
+        self.add_balls == 0
+
+    def pachinko(probability, nim_traials):
+        win_flg == #####
+
+    # Bool(1/319)
+    def hidariUti(probability==319):
+        print(にげちゃだめだにげちゃだめｄにげちゃだめにげちゃだｍ) # 煽る
+        if random.randrange(probability)==1:
+            return true
+        else: return false
+
+    # ()
+    def chanceTime():
+        if self.hidariUti(): # HU Atari
+            pass
+        else: # HU Hazure:
+            for i in 100:
+                
+            
+        # IN case of Atari
+        self.add_balls == 450
+        # if random.randrange(100)  0~1
+        if rand(random.random(), 2) <= 58: #[0,1,~,100]
+            return true
+        else:
+            # chance
+            ~~~
+
+
+    # ST.()
+    def rush():
+        if self.chanceTime():
+        
+        else: false
+
+
+        
+
+
+
+if __name__=="__main__":
+    print(初期残高は？)
+    input_balance = input("Inicial Balance Amount:")
+    print(f'your balance:{input_balance}\nBall Price: 4 yen')
+
+    new_eva15_machine = Eva15(input_balance)
+    
